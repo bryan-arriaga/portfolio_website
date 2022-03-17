62320077c555f5fe5c64f66680bfd227d64ff9cf
aHumptyDumpty <bryanarriaga.arriaga@gmail.com>
2022-03-16 19:51:10 -0500

    change technologies experience section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,8 +17,7 @@ const Header = () => (
   <Container>
     <Div1>
       <Link href="/">
-        <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: "20px" }}>
-          
+        <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: "20px" }}> 
         </a>
       </Link>
     </Div1>
