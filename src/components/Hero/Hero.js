f406b6a447bf1430de5642c574097362ad607dc3
aHumptyDumpty <bryanarriaga.arriaga@gmail.com>
2022-05-26 23:07:34 -0500

    'change to hero component'

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { Anchor, LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { Anchor, LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
   <Section row nopadding>
-  <LeftSection>
-    <SectionTitle main center>
-      Bryan - Software Engineer <br />
-    </SectionTitle>
-    <SectionText>
-      Hi, my name is Bryan, I am a Software Engineer based in Chicago with 2 years of professional experience. 🧑🏻‍💻 I am currently working @ Vanguard!
-      <br />
-      For any queries please feel free to contact me!
- 
-    </SectionText>
-    <Anchor href = "mailto:arriagabryan@gmail.com"> <Button>Contact me</Button></Anchor>
-
-  </LeftSection>
-
+    <LeftSection>
+      <SectionTitle main center>
+        Bryan Arriaga - Software Engineer <br />
+      </SectionTitle>
+      <SectionText>
+        Hi, my name is Bryan, I am a Software Engineer based in Chicago with 2
+        years of professional experience. 🧑🏻‍💻 I am currently working @ Vanguard!
+        <br />
+        For any queries please feel free to contact me!
+      </SectionText>
+      <Anchor href="mailto:arriagabryan@gmail.com">
+        {" "}
+        <Button>Contact me</Button>
+      </Anchor>
+    </LeftSection>
   </Section>
 );
 
